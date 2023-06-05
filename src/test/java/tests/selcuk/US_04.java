a7bffac5fc4d6addfe7ea4510c0ba1a5a8348abd
SELCUKKIBAR <selcukkibar88@gmail.com>
2023-06-05 23:13:12 +0300

    selcuk_05/06_v3

--- src/test/java/tests/selcuk/US_04.java
+++ src/test/java/tests/selcuk/US_04.java
@@ -1,4 +1,56 @@
 package tests.selcuk;
 
-public class US_04 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.animation.model.KeyframeStyle;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AdminDashboard;
+import pages.UserHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US_04{
+
+    @Test
+    public void testCase01(){
+
+
+        //TEST CASE 1
+        //1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
+        Driver.getDriver().get(ConfigReader.getProperty("trpURL"));
+
+
+        //2- Kullanıcı footer bölümüne iner
+        UserHomePage userHomePage = new UserHomePage();
+
+
+        //3- Kullanıcı 3 DAYS IN BUENOS AIRES'i tıklar
+
+        userHomePage.threeDaysInBuenosAires.click();
+
+        //4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "3 days in Buenos Aires";
+
+        Assert.assertTrue(actualTitle.equals(expectedTitle));
+
+
+        Driver.quitDriver();
+    }
+
 }
+
+
+
+/*
+TEST CASE 1
+1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
+2- Kullanıcı footer bölümüne iner
+3- Kullanıcı 3 DAYS IN BUENOS AIRES'i tıklar
+4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
+
+
+ */
