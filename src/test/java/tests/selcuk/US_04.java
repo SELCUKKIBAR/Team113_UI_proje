c316dcffd7a5c8cb71c862b2d669472f9ce6fb2f
SELCUKKIBAR <selcukkibar88@gmail.com>
2023-06-06 03:18:09 +0300

    selcuk_06/06_v3

--- src/test/java/tests/selcuk/US_04.java
+++ src/test/java/tests/selcuk/US_04.java
@@ -12,16 +12,20 @@ import pages.AdminDashboard;
 import pages.UserHomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.TestBaseRapor;
 
-public class US_04{
+public class US_04 extends TestBaseRapor {
 
     @Test
     public void testCase01(){
 
+        extentTest = extentReports.createTest("Tripandway FOOTER testi",
+                "Kullanici açılan sayfanın Title'sinin '3 days in Buenos Aires' oldugunu test eder");
 
         //TEST CASE 1
         //1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
         Driver.getDriver().get(ConfigReader.getProperty("trpURL"));
+        extentTest.info("Kullanici Tripandway anasayfaya gider...");
 
 
         //2- Kullanıcı footer bölümüne iner
@@ -31,12 +35,14 @@ public class US_04{
 
         //3- Kullanıcı 3 DAYS IN BUENOS AIRES'i tıklar
         userHomePage.threeDaysInBuenosAires.click();
+        extentTest.info("Kullanici 3 days in Buenos Aires'a tıklar...");
 
         //4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "3 days in Buenos Aires";
 
         Assert.assertTrue(actualTitle.equals(expectedTitle));
+        extentTest.pass("Sayfanın Title'ının '3 days in Buenos Aires' olduğunu doğrular...");
 
 
         Driver.quitDriver();
@@ -45,9 +51,13 @@ public class US_04{
     @Test
     public void testCase02(){
 
+        extentTest = extentReports.createTest("Tripandway FOOTER testi",
+                "Kullanici açılan sayfanın Title'sinin '10 days in Buenos Aires' oldugunu test eder");
+
         //TEST CASE 2
         //1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
         Driver.getDriver().get(ConfigReader.getProperty("trpURL"));
+        extentTest.info("Kullanici Tripandway anasayfaya gider...");
 
         //2- Kullanıcı footer bölümüne iner
         UserHomePage userHomePage = new UserHomePage();
@@ -56,12 +66,14 @@ public class US_04{
 
         //3- Kullanıcı 10 DAYS IN BUENOS AIRES'i tıklar
         userHomePage.tenDaysInBuenosAires.click();
+        extentTest.info("Kullanici 10 days in Buenos Aires'a tıklar...");
 
         //4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "10 days in Buenos Aires";
 
         Assert.assertTrue(actualTitle.equals(expectedTitle));
+        extentTest.pass("Sayfanın Title'ının '10 days in Buenos Aires' olduğunu doğrular...");
 
 
         Driver.quitDriver();
@@ -71,9 +83,15 @@ public class US_04{
 
     @Test
     public void testCase03(){
+
+        extentTest = extentReports.createTest("Tripandway FOOTER testi",
+                "Kullanici açılan sayfanın Title'sinin '3 days in Bangkok' oldugunu test eder");
+
+
         //TEST CASE 3
         //1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
         Driver.getDriver().get(ConfigReader.getProperty("trpURL"));
+        extentTest.info("Kullanici Tripandway anasayfaya gider...");
 
         //2- Kullanıcı footer bölümüne iner
         UserHomePage userHomePage = new UserHomePage();
@@ -82,12 +100,14 @@ public class US_04{
 
         //3- Kullanıcı 3 DAYS IN BANGKOK'u tıklar
         userHomePage.threeDaysInBangkok.click();
+        extentTest.info("Kullanici 3 days in Bangkok'a tıklar...");
 
         //4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "3 days in Bangkok";
 
         Assert.assertTrue(actualTitle.equals(expectedTitle));
+        extentTest.pass("Sayfanın Title'ının '3 days in Bangkok' olduğunu doğrular...");
 
 
         Driver.quitDriver();
@@ -96,9 +116,15 @@ public class US_04{
 
     @Test
     public void testCase04(){
+
+        extentTest = extentReports.createTest("Tripandway FOOTER testi",
+                "Kullanici açılan sayfanın Title'sinin '7 days in Salina Island' oldugunu test eder");
+
+
         //TEST CASE 4
         //1- Kullanıcı https://qa.tripandway.com/ ana sayfasına gider
         Driver.getDriver().get(ConfigReader.getProperty("trpURL"));
+        extentTest.info("Kullanici Tripandway anasayfaya gider...");
 
         //2- Kullanıcı footer bölümüne iner
         UserHomePage userHomePage = new UserHomePage();
@@ -107,12 +133,14 @@ public class US_04{
 
         //3- Kullanıcı 7 DAYS IN SALINA ISLAND'ı tıklar
         userHomePage.sevenDaysInSalinaIslandLatestPackages.click();
+        extentTest.info("Kullanici 7 days in Salina Island'a tıklar...");
 
         //4- Kullanıcı sayfanın tıklandığını titilina bakarak test eder
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "7 days in Salina Island";
 
         Assert.assertTrue(actualTitle.equals(expectedTitle));
+        extentTest.pass("Sayfanın Title'ının '7 days in Salina Island' olduğunu doğrular...");
 
 
         Driver.quitDriver();
