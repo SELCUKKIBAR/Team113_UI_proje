0e021e6193b14ea5621f7884350becb7eed9a1b1
mehmetsaidpelit <you@example.com>
2023-06-09 17:01:39 -0300

    mehmet 09/06 tum testler

--- src/test/java/tests/mehmet/US_38.java
+++ src/test/java/tests/mehmet/US_38.java
@@ -10,8 +10,9 @@ import pages.UserDashboard;
 import pages.UserHomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.SelcukTestBaseRapor;
 
-public class US_38 {
+public class US_38  extends SelcukTestBaseRapor {
     AdminDashboard adminDashboard=new AdminDashboard();
     @BeforeTest
     public void setup(){
@@ -22,17 +23,29 @@ public class US_38 {
     @Test
     public void AdminGirisiYapveHesapDegistirmeGoruntele(){
 
+
+        extentTest = extentReports.createTest("Admin Girisi ve Hesap Degistirme Goruntuleme",
+                "Admin girisi yapilir ve hesap degistirilir");
+
         WebElement emailKutusu= adminDashboard.adminDashboardEmailKutusu;
 
         emailKutusu.sendKeys("admin04@tripandway.com");
+
+        extentTest.info("Admin bolumune e-mail adresi girilir.");
 
         WebElement passwordKutusu=adminDashboard.adminDashboardPasswordKutusu;
 
         passwordKutusu.sendKeys("123123123");
 
+        extentTest.info("Admin bolumune password girilir.");
+
         adminDashboard.adminDashboardLoginButonu.click();
 
+        extentTest.info("Login tusuna tiklanilir.");
+
         Assert.assertTrue(adminDashboard.admin04sagustbuton.isDisplayed());
+
+        extentTest.pass("Admin hesabi olusturup hesap degistirme goruntule testi.");
 
         Driver.quitDriver();
 
